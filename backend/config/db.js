87e17fafc53058e6e7287061fb063c836b7e776a
Sheikh Fareed <fareedalinehan@gmail.com>
2025-06-12 02:17:25 +0500

    connected with db altas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,15 @@
-const mongoose = require("mongoose");
+// config/db.js
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+  
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error('MongoDB connection failed:', err.message);
     process.exit(1);
   }
 };
--- backend/models/product.js
+++ backend/models/product.js
@@ -80,4 +80,4 @@ const productSchema = new mongoose.Schema({
   timestamps: true
 });
 
-module.exports = mongoose.model("Product", productSchema);
+module.exports = mongoose.model("product", productSchema);
