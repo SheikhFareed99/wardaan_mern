9a416b42e79f6f8f1d97c57353947740d787ce57
Sheikh Fareed <fareedalinehan@gmail.com>
2025-07-13 02:26:46 +0500

    s

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -272,7 +272,7 @@ function Home() {
       {/* Video Section */}
       <div className="mb-12 sm:mb-16 md:mb-20 lg:mb-24">
         <video
-          src="https://res.cloudinary.com/dswff96z5/video/upload/v1752351108/IMG_0972_ddwwud.mov"
+          src="https://res.cloudinary.com/dswff96z5/video/upload/v1752355578/WhatsApp_Video_2025-07-13_at_01.09.46_ad010ed7_nyp6pg.mp4"
           autoPlay
           loop
           muted
