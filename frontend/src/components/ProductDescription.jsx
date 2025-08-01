3cec1e1511eb687370c4dac2a24afe5a2e11da84
Sheikh Fareed <fareedalinehan@gmail.com>
2025-08-02 02:08:20 +0500

    s

--- frontend/src/components/ProductDescription.jsx
+++ frontend/src/components/ProductDescription.jsx
@@ -89,7 +89,7 @@ function ProductDescription() {
   };
 
   const handleAddToCart = () => {
-    
+
     if (typeof fbq !== "undefined") {
       fbq('track', 'AddToCart', {
         content_ids: [id], 
@@ -319,7 +319,7 @@ function ProductDescription() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
                 </svg>
-                <span className="text-sm">Easy Returns & Refunds within 3 Days</span>
+                <span className="text-sm">Easy Returns & Refunds within 7 Days</span>
               </motion.div>
             </div>
 
